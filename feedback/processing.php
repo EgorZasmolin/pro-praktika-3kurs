ebd607cc17600795c8e5d681aeca9b5d8dc67299
Егор Засмолин <Zasmolin.e@gmail.com>
2022-12-06 17:19:57 +0300

    fourth commit

--- feedback/processing.php
+++ feedback/processing.php
@@ -1,19 +1,11 @@
 <?php
-/* Здесь проверяется существование переменных */
-if (isset($_POST['name'])) {$phone = $_POST['name'];}
-if (isset($_POST['email'])) {$name = $_POST['email'];}
- 
-/* Сюда впишите свою эл. почту */
-$myaddres  = "zasmolin.e@gmail.com"; // кому отправляем
- 
-/* А здесь прописывается текст сообщения, \n - перенос строки */
-// $mes = "Тема: Заказ обратного звонка!\nТелефон: $phone\nИмя: $name";
- 
-/* А эта функция как раз занимается отправкой письма на указанный вами email */
-$sub='Заказ'; //сабж
-$email='Заказ обратного звонка'; // от кого
-$send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
- 
-ini_set('short_open_tag', 'On');
-header('Refresh: 3; URL=index.html');
+$to = "zasmolin.e@gmail.com"; // емайл получателя данных из формы
+$tema = "Форма обратной связи на PHP"; // тема полученного емайла
+$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
+$message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
+$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
+$message .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
+$headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
 ?>
